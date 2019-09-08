1cc252bfa9245680e514b0c50e45d754051eeea7
Connor Strand <42255832+Strandxo@users.noreply.github.com>
2019-09-08 22:17:51 +0930

    updating meta information.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,25 +1,25 @@
-const sidebar = require('./sidebar.js');
+const sidebar = require("./sidebar.js");
 
 const config = {
     title: "Shield Documentation",
     description: "The official documentation for shield. A bot designed to aid with your moderational needs.",
     head: [
-		['link', { rel: 'icon', href: '/favicon.png' }],
-		['meta', { name: 'og:title', content: 'Shield Documentation' }],
-		['meta', { name: 'og:description', content: 'Official documentation for Shield, guild moderation and settings bot.' }],
-		['meta', { name: 'og:type', content: 'website' }],
-		['meta', { name: 'og:url', content: 'https://discordjs.guide/' }],
-		['meta', { name: 'og:locale', content: 'en_US' }],
+		["link", { rel: "icon", href: "/favicon.png" }],
+		["meta", { name: "og:title", content: "Shield Documentation" }],
+		["meta", { name: "og:description", content: "Official documentation for Shield, guild moderation and settings bot." }],
+		["meta", { name: "og:type", content: "website" }],
+		["meta", { name: "og:url", content: "https://shieldbot.ga/" }],
+		["meta", { name: "og:locale", content: "en_US" }],
 	],
     theme: "yuu",
-    repo: 'strandxo/shield-docs',
-    docsDir: 'src',
+    repo: "strandxo/shield-docs",
+    docsDir: "src",
     editLinks: true,
     sidebarDepth: 3,
     lastUpdated: true,
     themeConfig: {
         yuu: {
-			colorThemes: ['blue', 'red'],
+			colorThemes: ["blue", "red"],
 		},
         nav: [
             { text: "Home", link: "/" },
