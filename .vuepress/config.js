e8bdf54965b2b0a896153a6427c08bbf852e7d7b
Connor Strand <42255832+Strandxo@users.noreply.github.com>
2019-09-10 00:05:41 +0930

    Update config.js

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,14 +1,15 @@
 const sidebar = require("./sidebar.js");
+const nav = require("./navbar")
 
 const config = {
-    title: "Shield Documentation",
-    description: "The official documentation for shield. A bot designed to aid with your moderational needs.",
+    title: "Shield",
+    description: "The official website for shield. A bot designed to aid with your moderational needs.",
     head: [
 		["link", { rel: "icon", href: "/favicon.png" }],
-		["meta", { name: "og:title", content: "Shield Documentation" }],
-		["meta", { name: "og:description", content: "Official documentation for Shield, guild moderation and settings bot." }],
+		["meta", { name: "og:title", content: "Shield Website" }],
+		["meta", { name: "og:description", content: "Official website for Shield, guild moderation and settings bot." }],
 		["meta", { name: "og:type", content: "website" }],
-		["meta", { name: "og:url", content: "https://shieldbot.ga/" }],
+		["meta", { name: "og:url", content: "https://shieldbot.site/" }],
 		["meta", { name: "og:locale", content: "en_US" }],
 	],
     theme: "yuu",
@@ -19,15 +20,9 @@ const config = {
     lastUpdated: 'Last Updated',
     themeConfig: {
         yuu: {
-			colorThemes: ["blue", "red"],
+            disableThemeIgnore: true,
 		},
-        nav: [
-            { text: "Home", link: "/" },
-            { text: "Documentation", link: "/docs/" },
-            { text: "Invite", link: "https://discordapp.com/api/oauth2/authorize?client_id=568698045412409344&permissions=8&scope=bot" },
-            { text: "Discord", link: "https://discord.gg/YGCuzUz" },
-            { text: "Twitter", link: "https://www.twitter.com/shieldbottweets" },
-        ],
+        nav,
         sidebar,
     }
 };
