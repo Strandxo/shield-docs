46feae63fa99021d4d04dd31a356c2544b36d1c0
Connor Strand <42255832+Strandxo@users.noreply.github.com>
2019-09-10 00:08:20 +0930

    Change of Socials to Support

--- .vuepress/navbar.js
+++ .vuepress/navbar.js
@@ -9,7 +9,7 @@ module.exports = [
         { text: "Bots For Discord", link: "https://botsfordiscord.com/bot/568698045412409344"},
         { text: "Bot List Space", link: "https://botlist.space/bot/568698045412409344"},
     ]},
-    { text: "Socials", items: [
+    { text: "Support", items: [
         { text: "Discord", link: "https://discord.gg/YGCuzUz" },
         { text: "Twitter", link: "https://www.twitter.com/shieldbottweets" },
         { text: "Donate", link: "https://www.patreon.com/" }
