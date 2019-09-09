4b52dbec6155a12e6ce4f49de5888baae17c5446
Connor Strand <42255832+Strandxo@users.noreply.github.com>
2019-09-10 00:05:21 +0930

    removal of nav from config to external file

---
+++ .vuepress/navbar.js
@@ -0,0 +1,17 @@
+module.exports = [
+    { text: "Home", link: "/" },
+    { text: "Documentation", link: "/docs/" },
+    { text: "Invite", link: "https://discordapp.com/api/oauth2/authorize?client_id=568698045412409344&permissions=8&scope=bot" },
+    { text: "Vote", items: [
+        { text: "DiscordBots", link: "https://discordbots.org/bot/568698045412409344"},
+        { text: "Discord Bot List", link: "https://discordbotlist.com/bots/568698045412409344"},
+        { text: "Divine Discord Bots", link: "https://divinediscordbots.com/bot/568698045412409344"},
+        { text: "Bots For Discord", link: "https://botsfordiscord.com/bot/568698045412409344"},
+        { text: "Bot List Space", link: "https://botlist.space/bot/568698045412409344"},
+    ]},
+    { text: "Socials", items: [
+        { text: "Discord", link: "https://discord.gg/YGCuzUz" },
+        { text: "Twitter", link: "https://www.twitter.com/shieldbottweets" },
+        { text: "Donate", link: "https://www.patreon.com/" }
+    ]}
+]
